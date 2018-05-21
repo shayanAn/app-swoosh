0aadb9843c57313ffe974868813492fc2fae7003
Shayan ansari <shayan14912@gmail.com>
2018-05-21 16:34:27 +0530

    segue with coding

--- app-swoosh/app-swoosh/Controller/ViewController.swift
+++ app-swoosh/app-swoosh/Controller/ViewController.swift
@@ -9,9 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgimg: UIImageView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
